df37076ebbfccf6d948ec8804c15095f0ba9bb44
anubhav bisht <anubhavbisht98@gmail.com>
2024-04-29 11:22:11 +0530

    set up a file server.Also learnt about readdir of fs

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -1,25 +1,48 @@
-/**
-  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
-  - Use built in Node.js `fs` module
-
-  The expected API endpoints are defined below,
-  1. GET /files - Returns a list of files present in `./files/` directory
-    Response: 200 OK with an array of file names in JSON format.
-    Example: GET http://localhost:3000/files
-
-  2. GET /file/:filename - Returns content of given file by name
-     Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-     Example: GET http://localhost:3000/file/example.txt
-
-    - For any other route not defined in the server return 404
-
-    Testing the server - run `npm run test-fileServer` command in terminal
- */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 const app = express();
+const portNumber = 3000;
 
+const filesPath = path.join(process.cwd(), "/files");
+app.get("/files", async (req, res) => {
+  try {
+    const allFiles = await fs.readdir(filesPath);
+    if (allFiles) {
+      return res.status(200).json(allFiles);
+    }
+    res.status(500).json({ error: "Failed to retrieve files" });
+  } catch (e) {
+    console.error("Error found in get route /files", e);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/file/:filename", async (req, res) => {
+  try {
+    const { filename } = req.params;
+    const allFiles = await fs.readdir(filesPath);
+    if (!allFiles.includes(filename)) {
+      res.status(404).send("File not found");
+      return;
+    }
+    const fileContent = await fs.readFile(
+      filesPath.concat(`/${filename}`),
+      "utf-8"
+    );
+    res.status(200).send(fileContent);
+  } catch (e) {
+    console.error("Error found in get route /files", e);
+    res.status(500).json({ error: "Failed to retrieve files" });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(portNumber, () => {
+  console.log(`File server listening on port number ${portNumber}`);
+});
 
 module.exports = app;
